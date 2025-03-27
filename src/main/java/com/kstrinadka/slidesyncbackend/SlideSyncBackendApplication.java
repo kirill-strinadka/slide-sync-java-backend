36787f4c4798cf7add38ddfc33c9468305d788eb
kstrinadka <kstrinadka@igatec.com>
2025-03-27 16:35:12 +0700

    init

---
+++ src/main/java/com/kstrinadka/slidesyncbackend/SlideSyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kstrinadka.slidesyncbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlideSyncBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlideSyncBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kstrinadka/slidesyncbackend/SlideSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kstrinadka.slidesyncbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlideSyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
